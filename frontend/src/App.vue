313625c7ceebcd555628f1a641890f298c227e65
ShAvEnRaVeN <waynef_1985@yahoo.co.uk>
2025-08-05 20:22:29 +0100

    Inital commit for frontend and backend

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <SetButtons />
+  </div>
+</template>
+
+<script>
+import SetButtons from './components/SetButtons.vue';
+export default {
+  name: 'App',
+  components: {
+    SetButtons
+  }
+};
+</script>
+
+<style></style>
---
+++ frontend/src/components/SetButtons.vue
@@ -0,0 +1,18 @@
+<template>
+
+        <div class="row bg-secondary text-white">
+            <div class="col-6 col-md-2">
+                <button type="button" class="btn btn-primary">OP01</button>
+            </div>
+            <div class="col-6 col-md-2">
+                <button type="button" class="btn btn-primary">OP02</button>
+            </div>
+            <div class="col-6 col-md-2">
+                <button type="button" class="btn btn-primary">OP03</button>
+            </div>
+            <div class="col-6 col-md-2">
+                <button type="button" class="btn btn-primary">OP04</button>
+            </div>
+        </div>
+
+</template>
